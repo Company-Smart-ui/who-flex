dda2a87c0391212cec21640870afccd64dab0c3a
Nataliia_Matviienko <92440177+Company-Smart-ui@users.noreply.github.com>
2023-01-06 22:56:01 +0200

    fixed menu 06.01

---
+++ src/js/slider-init.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".MenuSwiperBanner", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor:'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
